055a840994944672232544edc9e1e732fa39ca37
Yuri Yaryshev <yuyaryshev@gmail.com>
2023-12-17 09:07:26 +0300

    First commit

---
+++ src/lesson01.ts
@@ -0,0 +1,14 @@
+console.log('Привет ')
+
+let nsa = 120
+
+while (nsa < 200) {
+    nsa = nsa + 35
+    console.log(nsa)
+}
+
+if (5 > 8) {
+    console.log('Зеленый')
+} else {
+    console.log('Красный')
+}
